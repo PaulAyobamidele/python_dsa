1774aba81ea56d6c85ea3f825d3f33973681c2d3
Ayobamidele <paulikeystreble@gmail.com>
2024-02-19 14:03:31 +0100

    Double Linked list

---
+++ pythondsa/study/doublelinkedlist.py
@@ -0,0 +1,18 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+node1 = Node(1)
+node2 = Node(2)
+node3 = Node(3)
+
+node1.next = node2
+node2.next = node3
+
+
+currentnode = node1
+while currentnode:
+    print(currentnode.data)
+    currentnode = currentnode.next
